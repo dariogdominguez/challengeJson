cc364943b52f2ef3dad09e94535bf7c048e62881
dariogdominguez <dominguez.dario@gmail.com>
2022-07-01 13:29:36 -0300

    segundo challenge 2 commit

---
+++ ch2/js/api.js
@@ -0,0 +1,59 @@
+const endPointUno = "https://jsonplaceholder.typicode.com/users"
+const endPointDos = "https://jsonplaceholder.typicode.com/users/1/albums"
+
+const getUsers = async(cb) => {
+
+    let data = await cb(endPointUno)
+    let copy = [...data]
+        mapUsers(copy);
+}
+
+const getAlbums = async(...params) => {
+let [id, cb] = params
+
+    let data = await cb(endPointDos);
+    let albums = [...data];
+    let filtered = filterById(id, albums)
+
+console.log(filtered);
+}
+
+const forma1 = async(endPoint) =>{
+    return fetch(endPoint)
+    .then(response => response.json())
+    .then(data => data);
+}
+
+const forma2 = async(endPoint) =>{
+    const json = await fetch(endPoint);
+    const data = await json.json();
+    return data;
+}
+
+function mapUsers(copy) {
+    let newArray = [];
+    copy.map(e =>{
+        let {address :{city, geo, street, suite}, company, email, id, name, username, website} = e
+
+        address = {city, geo :{lng :geo.lng}, street, suite }
+
+        newObj = {
+            address,
+            company,
+            email,
+            id,
+            name,
+            website,
+        }
+        newArray.push(newObj);
+    })
+    console.log(newArray);
+}
+
+function filterById(...params) {
+    let [id, albums] = params
+    return albums.filter(e => e.id === id);
+}
+
+getUsers(forma1);
+getAlbums(3, forma2);
